5d26353393d2541a580580291f4b5923534fcf61
INarvaezR <isared.narvaez@gmail.com>
2025-11-30 11:37:23 -0500

    Ingreso de datos en CreitsData.js

---
+++ src/data/creditsData.js
@@ -0,0 +1,68 @@
+export const creditsData = [
+  {
+    id: 1, 
+    name: "Crédito de libre inversión",
+    descripción: "Dinero más rápido",
+    minAmount: 1000000,
+    maxAmount: 30000000,
+    interestRate: 1.6,
+    maxTerm: 60,
+    requirements: "Mayor de 18 amños, ingresos mínimos de $1.200.000",
+    icon:""
+ },
+ {
+    id: 2, 
+    name: "Crédito de Vehículo",
+    descripción: "Dinero más rápido",
+    minAmount: 1000000,
+    maxAmount: 30000000,
+    interestRate: 1.8,
+    maxTerm: 60,
+    requirements: "Mayor de 18 amños, ingresos mínimos de $1.200.000",
+    icon:""
+ },
+ {
+    id: 3, 
+    name: "Crédito de Vivienda",
+    descripción: "Dinero más rápido",
+    minAmount: 10000000,
+    maxAmount: 300000000,
+    interestRate: 1.2,
+    maxTerm: 60,
+    requirements: "Mayor de 18 amños, ingresos mínimos de $1.200.000",
+    icon:""
+ },
+ {
+    id: 4, 
+    name: "Crédito Educativo",
+    descripción: "Dinero más rápido",
+    minAmount: 2000000,
+    maxAmount: 500000000,
+    interestRate: 1.5,
+    maxTerm: 60,
+    requirements: "Mayor de 18 amños, ingresos mínimos de $1.200.000",
+    icon:""
+ },
+ {
+    id: 5, 
+    name: "Crédito Empresarial",
+    descripción: "Dinero más rápido",
+    minAmount: 6000000,
+    maxAmount: 450000000,
+    interestRate: 1.9,
+    maxTerm: 60,
+    requirements: "Mayor de 18 amños, ingresos mínimos de $1.200.000",
+    icon:""
+ },
+ {
+    id: 6, 
+    name: "Crédito de Consumo",
+    descripción: "Dinero más rápido",
+    minAmount: 2000000,
+    maxAmount: 650000000,
+    interestRate: 1.7,
+    maxTerm: 60,
+    requirements: "Mayor de 18 amños, ingresos mínimos de $1.200.000",
+    icon:""
+ },
+]
