0434baba7edbb3fb618d7e9740ab5824eec7ba69
INarvaezR <isared.narvaez@gmail.com>
2025-12-02 00:02:59 -0500

    Agregación de datos a CreditCard

--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 
-const CreditCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export const CreditCard = ({ credit}) => {
+   
+    const {
+        name,
+        description,
+        minAmount,
+        maxAmount,
+        interestRate,
+        maxTern,
+        requirenents,
+        icon,
+    } = credit;
 
-export default CreditCard
+    return (
+        <div className='credit-card'>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <div className="detil-item">
+                    <span className="label">Plazo:</span>
+                    <span className="value">hasta {maxTern} meses</span>
+                </div>
+
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+
+                <div className="detil-item">
+                    <span className="label">Plazo:</span>
+                    <span className="value">hasta {maxTern} meses</span>
+                </div>
+                
+                <div className="detil-item">
+                    <span className="label">Plazo:</span>
+                    <span className="value">hasta {maxTern} meses</span>
+                </div>
+            </div>
+        </div>
+    )
+}
